ee03ad2a2fb5fb1fb1eeecb3ec6302c9bfa394ad
YaoxinShi <yaoxin.shi@intel.com>
2022-07-11 14:27:25 +0800

    [Mouse] init

---
+++ Mouse/Mouse.cpp
@@ -0,0 +1,78 @@
+﻿// Mouse.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <windows.h>
+
+void MouseMove(int x, int y)//鼠标移动到指定位置
+{
+	double fScreenWidth = ::GetSystemMetrics(SM_CXSCREEN) - 1;//获取屏幕分辨率宽度
+	double fScreenHeight = ::GetSystemMetrics(SM_CYSCREEN) - 1;//获取屏幕分辨率高度
+	double fx = x * (65535.0f / fScreenWidth);
+	double fy = y * (65535.0f / fScreenHeight);
+	INPUT  Input = { 0 };
+	Input.type = INPUT_MOUSE;
+	Input.mi.dwFlags = MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE;
+	Input.mi.dx = fx;
+	Input.mi.dy = fy;
+	SendInput(1, &Input, sizeof(INPUT));
+}
+
+void MouseLeftDown()//鼠标左键按下
+{
+	INPUT  Input = { 0 };
+	Input.type = INPUT_MOUSE;
+	Input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+	SendInput(1, &Input, sizeof(INPUT));
+}
+
+void MouseLeftUp()//鼠标左键放开
+{
+	INPUT  Input = { 0 };
+	Input.type = INPUT_MOUSE;
+	Input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+	SendInput(1, &Input, sizeof(INPUT));
+}
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Mouse.exe - show mouse position" << std::endl;
+	std::cout << "Mouse.exe <x> <y> - mouse click <x,y>" << std::endl;
+
+    if (argc == 1)
+    {
+        POINT p;
+        while (true)
+        {
+            GetCursorPos(&p);
+            printf("Cursor: (%d, %d)\n", p.x, p.y);
+            Sleep(1000);
+        }
+    }
+    else
+    {
+        int x = atoi(argv[1]);
+        int y = atoi(argv[2]);
+        SetCursorPos(x, y);
+		mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+		Sleep(10);
+		mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+
+		//new API
+		//MouseMove(100, 100);
+		//MouseLeftDown();
+		//Sleep(10);
+		//MouseLeftUp();
+    }
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
